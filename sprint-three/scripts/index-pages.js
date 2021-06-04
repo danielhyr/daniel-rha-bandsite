c8c149dbc9fca531ca03c2749981ba100c90e756
danielhyr <danhyunrha@gmail.com>
2021-06-04 01:02:05 -0400

    working on like buttons

--- sprint-three/scripts/index-pages.js
+++ sprint-three/scripts/index-pages.js
@@ -23,37 +23,117 @@ const makeElement = (elementName, className) => {
 // Writing the axios comment url and register
 const COMMENT_URL = "https://project-1-api.herokuapp.com/comments"
 const API_KEY = "47bba9b7-d7ec-48dc-a68a-93ec482f02de";
+
 // axios get request
 
+axios
+.get(`${COMMENT_URL}?api_key=${API_KEY}`)
+    .then(result => console.log(result))
+
+
+
+
+    function hello (comment) {
+    let likeButton = document.querySelector(".comments-pre-top__like")
+    likeButton.addEventListener('click', () => {
+        axios.put(`${COMMENT_URL}/${comment}/like?api_key=${API_KEY}`, {
+            "name": "Nigel",
+            "comment": "What a cool site",
+            "id": comment.id,
+            "likes": 1,
+            "timestamp": 1530744795832
+
+        }).then(() => {
+            document.querySelectorAll(".comments-pre").forEach(event => event.remove())
+            displayComment()
+
+
+        })
+
+    })
+
+}
 const displayComment = () => {
     axios
     .get(`${COMMENT_URL}?api_key=${API_KEY}`)
         .then(result => {
             const commentsArray = result.data
+            console.log(result.data)
             commentsArray.forEach((comment) => {
-                if (comment.name === "Connor Walton") {
-                    innerDisplay(comment, "top", "")
-            } else if (comment.name === "Emilie Beach") {
-                innerDisplay(comment, "middle", "")
-            } else if (comment.name === "Miles Acosta"){
-                innerDisplay(comment, "bottom", "")
-            } else {
-                innerDisplayFirst(comment, "new", "--pictureClass")
-            }
+                
+            //     if (comment.name === "Connor Walton") {
+            //         innerDisplay(comment, "top", "", comment.likes)
+
+            // } else if (comment.name === "Emilie Beach") {
+            //     innerDisplay(comment, "middle", "", comment.likes)
+
+            // } else if (comment.name === "Miles Acosta"){
+            //     innerDisplay(comment, "bottom", "", comment.likes)
+
+            // } else {
+                innerDisplayFirst(comment, "new", "--pictureClass", comment.likes)
+                if (comment.name === "Connor Walton") { 
+
+                }
+
+            
+            hello (comment.id)
+
+
+            // let likeButton = document.querySelector(".comments-pre-top__like")
+            // likeButton.addEventListener('click', () => {
+            //     axios.put(`${COMMENT_URL}/${comment.id}/like?api_key=${API_KEY}`, {
+            //         "name": "Nigel",
+            //         "comment": "What a cool site",
+            //         "id": comment.id,
+            //         "likes": 1,
+            //         "timestamp": 1530744795832
+
+            //     }).then(() => {
+            //         document.querySelectorAll(".comments-pre").forEach(event => event.remove())
+            //         displayComment()
+
+
+            //     })
+
+            // })
         })
 
         }).catch(error => console.log(error))
                 body.insertBefore(section, body.children[2])
 
+                
 }
 
 
-// Call function to initial load the three comments
+
+// function likeComments (selectLikeButton, commentURL, commentID, apiKey) {
+//     let likeButton = document.querySelector(selectLikeButton)
+//     likeButton.addEventListener('click', () => {
+//         axios.put(commentURL +`/${commentID}/like?api_key=` + apiKey, {
+//             "name": "Nigel",
+//             "comment": "What a cool site",
+//             "id": commentID,
+//             "likes": 1,
+//             "timestamp": 1530744795832
+
+//         }).then(() => {
+//             document.querySelectorAll(".comments-pre").forEach(event => event.remove())
+//             displayComment()
+
+//         })
+
+
+//     })
+
+// }
+
+
 displayComment()
 
 // Function innerdisplay to display comments
 
-function innerDisplay (comment, position, picture) {
+function innerDisplay (comment, position, picture, like) {
     let outerDiv = makeElement("div", "comments-pre");
     outerDiv.classList.add(`comments-pre--${position}Border`)
 
@@ -67,6 +147,15 @@ function innerDisplay (comment, position, picture) {
     innerP.innerText = comment.name
     innerDiv.append(innerP)
 
+    let innerLike = makeElement("button", "comments-pre-top__like")
+    innerLike.innerText = `Likes:${like}`
+    innerDiv.append(innerLike)
+
+    let innerDelete = makeElement("button", "comments-pre-top__delete")
+    innerDelete.innerText = "delete"
+    innerDiv.append(innerDelete)
+    
+    
     let innerP2 = makeElement("p", "comments-pre-top__date")
     let dateVar = new Date(Number(comment.timestamp))
     let dateFormatted =  ('0' + (dateVar.getMonth()+1)).slice(-2) + '/' + ('0' + dateVar.getDate()).slice(-2) + '/' + dateVar.getFullYear();
@@ -80,7 +169,7 @@ function innerDisplay (comment, position, picture) {
         return outerDiv
 }
 
-function innerDisplayFirst (comment, position, picture) {
+function innerDisplayFirst (comment, position, picture, like) {
     let outerDiv = makeElement("div", "comments-pre");
     outerDiv.classList.add(`comments-pre--${position}Border`)
 
@@ -93,6 +182,15 @@ function innerDisplayFirst (comment, position, picture) {
     let innerP = makeElement("p", "comments-pre-top__name")
     innerP.innerText = comment.name
     innerDiv.append(innerP)
+
+    
+    let innerLike = makeElement("button", "comments-pre-top__like")
+    innerLike.innerText = `Likes:${like}`
+    innerDiv.append(innerLike)
+
+    let innerDelete = makeElement("button", "comments-pre-top__delete")
+    innerDelete.innerText = "delete"
+    innerDiv.append(innerDelete)
 
     let innerP2 = makeElement("p", "comments-pre-top__date")
     let dateVar = new Date(Number(comment.timestamp))
@@ -108,7 +206,9 @@ function innerDisplayFirst (comment, position, picture) {
 
     section.insertBefore(outerDiv, section.children[0])
 
-        return outerDiv
+
+
+    return outerDiv
 }
 
 let form = document.querySelector('.comments-form')
@@ -171,4 +271,3 @@ function turnDate () {
 }
 
 
-// let dateTime = new Date(Number(item.timestamp))
